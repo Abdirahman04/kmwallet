9d370bc7c7c4dd5d110ac68749319bead0006807
Abdirahman04 <abdixcom@gmail.com>
2024-01-10 07:25:44 +0300

    added deposit endpoint

--- src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
+++ src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
@@ -5,10 +5,7 @@ import com.abdirahman.kmwallet.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,6 +50,12 @@ public class TransactionController {
     public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
         Transaction transaction = transactionService.getTransactionById(id);
         return  new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @PostMapping("/deposit/{id}")
+    public ResponseEntity<String> deposit(@PathVariable String id, @RequestBody Transaction transaction) {
+        transactionService.deposit(id, transaction);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
