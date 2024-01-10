ec67b76384473f1796a08153284b108c2d569b6d
Abdirahman04 <abdixcom@gmail.com>
2024-01-10 07:19:24 +0300

    added getTransactionById endpoint

--- src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
+++ src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kmwallet")
+@RequestMapping("/kmwallet/transaction")
 public class TransactionController {
     TransactionService transactionService;
 
@@ -25,27 +25,35 @@ public class TransactionController {
     public TransactionController() {
     }
 
-    @GetMapping("/transaction")
+    @GetMapping("/")
     public ResponseEntity<List<Transaction>> getAllTransactions() {
         List<Transaction> transactions = transactionService.getAllTransactions();
         return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
 
-    @GetMapping("/transaction/{type}")
+    @GetMapping("/type/{type}")
     public ResponseEntity<List<Transaction>> getAllTransactionsByType(@PathVariable String type) {
         List<Transaction> transactions = transactionService.getAllTransactionsByType(type);
         return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
 
-    @GetMapping("/transaction/account/{id}")
+    @GetMapping("/account/{id}")
     public ResponseEntity<List<Transaction>> getAllTransactionsByAccount(@PathVariable String id) {
         List<Transaction> transactions = transactionService.getAllTransactionsByAccount(id);
         return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
 
-    @GetMapping("/transaction/target/{id}")
+    @GetMapping("/target/{id}")
     public ResponseEntity<List<Transaction>> getAllTransactionsByTarget(@PathVariable String id) {
         List<Transaction> transactions = transactionService.getAllTransactionsByTarget(id);
         return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
+        Transaction transaction = transactionService.getTransactionById(id);
+        return  new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+
 }
