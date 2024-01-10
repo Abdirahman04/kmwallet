9cf37cb6cf7a781d0d28cf7ef662641cbbf803ea
Abdirahman04 <abdixcom@gmail.com>
2024-01-10 07:16:00 +0300

    added some transaction endpoint

--- src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
+++ src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,21 @@ public class TransactionController {
         return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
 
+    @GetMapping("/transaction/{type}")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByType(@PathVariable String type) {
+        List<Transaction> transactions = transactionService.getAllTransactionsByType(type);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
 
+    @GetMapping("/transaction/account/{id}")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByAccount(@PathVariable String id) {
+        List<Transaction> transactions = transactionService.getAllTransactionsByAccount(id);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @GetMapping("/transaction/target/{id}")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByTarget(@PathVariable String id) {
+        List<Transaction> transactions = transactionService.getAllTransactionsByTarget(id);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
 }
