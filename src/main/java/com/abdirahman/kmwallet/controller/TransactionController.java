fd5b88cfcd4466dac0daa17998335dff57ac35bf
Abdirahman04 <abdixcom@gmail.com>
2024-01-10 07:12:40 +0300

    added getAllTransactions endpoint

--- src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
+++ src/main/java/com/abdirahman/kmwallet/controller/TransactionController.java
@@ -1,9 +1,15 @@
 package com.abdirahman.kmwallet.controller;
 
+import com.abdirahman.kmwallet.model.entity.Transaction;
 import com.abdirahman.kmwallet.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/kmwallet")
@@ -16,6 +22,12 @@ public class TransactionController {
     }
 
     public TransactionController() {
+    }
+
+    @GetMapping("/transaction")
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
     }
 
 
